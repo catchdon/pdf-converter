0c20d050f900ae2c00116b4c5b2caa397c066103
박종현 <bagjonghyeon@bagjonghyeon-ui-MacBookAir.local>
2025-07-19 01:27:19 +0900

    3771366 제

--- app/layout.jsx
+++ app/layout.jsx
@@ -2,6 +2,7 @@ import { Inter } from "next/font/google"
 import "./globals.css"
 import { Header } from "@/components/header"
 import { Footer } from "@/components/footer"
+import { Analytics } from "@vercel/analytics/next"
 
 const inter = Inter({ subsets: ["latin"] })
 
@@ -59,6 +60,7 @@ export default function RootLayout({ children }) {
         <Header />
         <main className="min-h-screen">{children}</main>
         <Footer />
+        <Analytics />
       </body>
     </html>
   );
